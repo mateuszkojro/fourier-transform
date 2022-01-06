3d8cfe8b27e397ceeb1eddac7782772c38e52c39
Mateusz Kojro <mateuszkojro@outlook.com>
2022-01-06 16:20:58 +0100

    Simple sin combinations agree with the same values in python

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,69 @@
+#include <complex>
+#include <vector>
 
-#include <iostream>
+#include "plot.h"
+#include "ranges.h"
 
-int main(){
-  std::cout << "Hello world!" << std::endl;
-  return 0;
+const double kPi = 3.1415;
+
+std::complex<double> dft(const std::vector<double>& signal, double freq) {
+  using namespace std::complex_literals;
+  std::complex<double> result(0, 0);
+  double N = signal.size();
+  for (int i = 0; i < signal.size(); i++) {
+	double n = i;
+	double k = freq;
+	double x = signal[i];
+	//	result += signal[i] * exp(-2 * kPi * 1i * n * freq);
+	//	result += signal[i] * exp(1i * n * freq);
+	result += x * 1. / N * exp(-(1i * n * k) / N);
+  }
+  return result;
+}
+
+int main(int argc, char* argv[]) {
+  int samples = 1000;
+
+  double from = 0;
+  auto wave1 =
+	  linspace([](double x) { return sin(x); }, from, 20 * kPi, samples);
+  auto wave2 =
+	  linspace([](double x) { return sin(x); }, from, 40 * kPi, samples);
+
+  auto wave_sum = wave1;
+  for (size_t i = 0; i < wave1.size(); i++) {
+	wave_sum[i] = wave1[i] + wave2[i];
+  }
+
+  std::vector<double> transformed, root, xs;
+  for (int x = 0; x < 200; x++){
+	xs.push_back(x);
+	transformed.push_back(abs(dft(wave_sum, x)));
+  }
+
+#ifdef MK_WITH_ROOT
+  TApplication app("app", &argc, argv);
+  TCanvas canvas("canvas", "Fourier transform", 10, 10, 640, 480);
+  canvas.Divide(1, 2);
+
+  TGraph transfomred_graph, original_graph;
+  transfomred_graph.SetMarkerStyle(7);
+  transfomred_graph.SetMarkerSize(2);
+
+
+  canvas.cd(1);
+  for (auto& pair : enumerate(wave_sum)) {
+	original_graph.AddPoint(pair.first, pair.second);
+  }
+  original_graph.Draw();
+
+  canvas.cd(2);
+  for (auto& pair : zip(xs, transformed)) {
+	transfomred_graph.AddPoint(pair.first, pair.second);
+  }
+
+  transfomred_graph.Draw();
+  canvas.Draw();
+  app.Run();
+#endif
 }
---
+++ src/ranges.h
@@ -0,0 +1,53 @@
+//
+// Created by M.Kojro on 1/6/2022.
+//
+
+#ifndef FOURIER_TRANSFORM_SRC_RANGES_H_
+#define FOURIER_TRANSFORM_SRC_RANGES_H_
+
+#include <cmath>
+
+/*
+ * !! This is not optimised for performance !!
+ */
+
+static std::vector<double> linspace(double (*f)(double), double from, double to,
+									int samples) {
+
+  std::vector<double> results;
+
+  double divisor = (to - from) / samples;
+  for (double x = from; x < to; x += divisor) { results.push_back(f(x)); }
+
+  return results;
+}
+
+std::vector<int64_t> range(int64_t from, int64_t to, int64_t step) {
+  std::vector<int64_t> result;
+  result.reserve((to - from) / step);
+  for (int64_t i = from; i < to; i += step) { result.push_back(i); }
+  return result;
+}
+
+std::vector<std::pair<double, double>> zip(const std::vector<double>& v1,
+										   const std::vector<double>& v2) {
+  std::vector<std::pair<double, double>> result(v1.size());
+
+  assert(v1.size() == v2.size());
+
+  for (size_t i = 0; i < v1.size(); i++) {
+	result[i] = std::make_pair(v1[i], v2[i]);
+  }
+
+  return result;
+}
+
+std::vector<std::pair<size_t, double>> enumerate(const std::vector<double>& v) {
+  std::vector<std::pair<size_t, double>> result(v.size());
+
+  for (size_t i = 0; i < v.size(); i++) { result[i] = std::make_pair(i, v[i]); }
+
+  return result;
+}
+
+#endif// FOURIER_TRANSFORM_SRC_RANGES_H_
